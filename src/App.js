ef179d6fa6225c1e92fffe902fd65bb510502fc1
Sumit Chouhan <sumit.chouhan@technoidentity.com>
2022-07-26 18:10:16 +0530

    ContextHook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ComponentA from "./components/ComponentA";
+import { createContext } from "react";
 
+export const UserContext = createContext();
+export const ChannelContext = createContext();
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserContext.Provider value={"Sumit"}>
+        <ChannelContext.Provider value={"Codevolution"}>
+          <ComponentA />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
     </div>
   );
 }
